2fd6d373a07a527c85db77ba8c2798cc94b0ea8f
Huynh Van Khai <vankhai.coder@gmail.com>
2025-02-27 15:01:07 +0700

    config folder

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase')
+        const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/rent-motobike')
         console.log(`Connect to mongodb successully : ${conn.connection.host}`);
 
     } catch (error) {
--- server.js
+++ server.js
@@ -3,35 +3,24 @@ import dotenv from 'dotenv'
 dotenv.config()
 import { connectDB } from './config/db.js'
 
-// import route : 
-// import authorRoutes from './routes/authorRoute.js'
-// import postRoutes from './routes/postRoute.js'
-
 // init app : 
 const app = express()
-
 // port : 
 const PORT = process.env.PORT || 5001
-
 // middleware : 
 app.use(express.json())
-
 // log request : 
 app.use((req, res, next) => {
     console.log(`Request Info : ${req.method} ${req.url}`);
     next()
 })
-// home page route : 
-app.get('/', (req, res) => {
-    res.json({ message: "app running success!" })
-})
-
-// customer route : 
-
-
 // listen port : 
 app.listen(PORT, (req, res) => {
     console.log(`App running on http://localhost:${PORT}`);
     // connect to db : 
     connectDB()
+})
+// home page route : 
+app.get('/', (req, res) => {
+    res.json({ message: "app running success!" })
 })
